dbe13493b4c5c0f50f47c25d1ff5df85e81d9d9a
deivendranbr <deivendranbr@gmail.com>
2020-04-23 16:52:17 +0530

    local storage changes

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -4,6 +4,7 @@ function Feeds(win, doc, container, url) {
 	this.url = url;
 	this.pageCount = 0;
 	this.container = container;
+	this.userId = 1; // Assuming user id is 1.
 
 	this.moreItemsBtn = this.doc.querySelector('.next-page');
 	this.onNextPageClickFn = this.onNextPageClick.bind(this);
@@ -11,14 +12,94 @@ function Feeds(win, doc, container, url) {
 	this.container.addEventListener('click', this.onTableClickFn, false);
 }
 
+// To get the domain name from url 
 function getDomainName(url) {
 	let urlParts = url.replace('http://','').replace('https://','').split(/[/?#]/);
 	
 	return urlParts[0];
 }
 
+
+// check whether the local storage data having the same record
+function isDataExists(array, id) {
+	let len = array.length;
+
+	for (let i = 0; i < len; i++) {
+		let data = array[i];
+
+		if (data.feedId === id) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+Feeds.prototype.saveHideData = function hideFn(id) {
+	// Check browser support
+	if (typeof(Storage) !== "undefined") {
+  		// Store
+
+  		if (localStorage.getItem("hidefeed") === null) {
+	  		localStorage.setItem("hidefeed", '[]');
+  		}
+
+  		let feedData = JSON.parse(localStorage.getItem("hidefeed"));
+  		if (isDataExists(feedData, id) === false) {
+	  		feedData.push({
+	  			user : this.userId,			// Assuming the user id is 1
+	  			feedId : id,
+	  			isHidden: true
+	  		});
+  		}
+		localStorage.setItem('hidefeed', JSON.stringify(feedData))
+	}
+}
+
+// saving upvote data to local storage
+Feeds.prototype.saveUpvote = function saveFn(id, targetRow) {
+	// Check browser support
+	if (typeof(Storage) !== "undefined") {
+  		// Store
+
+  		if (localStorage.getItem("upvotefeed") === null) {
+	  		localStorage.setItem("upvotefeed", '[]');
+  		}
+
+  		let feedData = JSON.parse(localStorage.getItem("upvotefeed"));
+  		if (isDataExists(feedData, id) === false) {
+	  		feedData.push({
+	  			user : this.userId,			// Assuming the user id is 1
+	  			feedId : id,
+	  			upvote: true
+	  		});
+	  		this.updateRow(targetRow);
+  		}
+		localStorage.setItem('upvotefeed', JSON.stringify(feedData))
+	}
+}
+
+// updating UI of row after upvote
+Feeds.prototype.updateRow = function updateFn(row) {
+	let upvote = row.querySelector('.points'),
+		upvoteCount = parseInt(upvote.textContent);
+
+	upvote.classList.add('upvoted');
+	upvote.innerHTML = '';
+	upvote.appendChild(this.doc.createTextNode((upvoteCount+1)))
+}
+
 Feeds.prototype.onTableClick = function onTableClickFn(e) {
 	let targetRow = e.target.closest('tr');
+
+	if(e.target.classList.contains('upvote-button') === true) {
+		this.saveUpvote(targetRow.dataset.id, targetRow);
+	}
+
+	if (e.target.classList.contains('hide-button') === true) {
+		this.saveHideData(targetRow.dataset.id);
+		targetRow.style.display = 'none';
+	}
 }
 
 Feeds.prototype.onNextPageClick = function clickFn(e) {
@@ -36,7 +117,7 @@ Feeds.prototype.render = function renderFn(data) {
 		let feed = hits[i];
 		let domainName = getDomainName(feed.url);
 
-		let listTemplate = `<tr class="list-item">
+		let listTemplate = `<tr data-id="${feed.objectID}" class="list-item">
 			<td><span class="comments">${feed.num_comments}</span><td>
 			<td><span class="points">${feed.points}</span></td>
 			<td><button aria-label="Upvote button" class="upvote-button"></button></td>
@@ -51,6 +132,13 @@ Feeds.prototype.render = function renderFn(data) {
 		let list = this.doc.createElement('tbody');
 
 		list.innerHTML = listTemplate;
+
+		// checks if user already upvoted feed
+		let feedData = JSON.parse(localStorage.getItem("upvotefeed"));
+		if (feedData !==null && isDataExists(feedData, feed.objectID) === true) {
+			this.updateRow(list.firstChild);
+		}
+
 		tbody.appendChild(list.firstChild);
 	}
 	this.container.innerHTML = '';
